5d39af4758e91467e8089110d76cfb4289956e56
masgun17 <masgun17@ku.edu.tr>
2022-06-07 00:11:21 +0300

    profile page fixes

--- my-app/src/Pages/ProfilePage.js
+++ my-app/src/Pages/ProfilePage.js
@@ -164,10 +164,10 @@ const ProfilePage = () => {
     }, 500);
   }, []);
 
-  useEffect(() => {
-    console.log(watchedListInfo, "watched");
-    console.log(wishlistInfo, "wish");
-  }, [watchedListInfo, wishlistInfo]);
+  //   useEffect(() => {
+  //     console.log(watchedListInfo, "watched");
+  //     console.log(wishlistInfo, "wish");
+  //   }, [watchedListInfo, wishlistInfo]);
 
   // const [genreOptions, setGenreOptions] = useState([]);
   // const [countryOptions, setCountryOptions] = useState([]);
@@ -309,13 +309,13 @@ const ProfilePage = () => {
           directors: director,
           actors: actor,
           flag: 0,
-          uid: uid
+          uid: uid,
         },
       ],
     };
     const result = await searchFromWatchWishListAction(jsonData);
     setWatchedListInfo(result);
-  }
+  };
 
   const onFilterWish = async () => {
     let duration_small = "0";
@@ -380,117 +380,117 @@ const ProfilePage = () => {
           directors: director,
           actors: actor,
           flag: 1,
-          uid: uid
+          uid: uid,
         },
       ],
     };
     const result = await searchFromWatchWishListAction(jsonData);
     setWishlistInfo(result);
-};
+  };
 
-    const onFilterBoth =  () => {
-        let duration_small = "0";
-        let duration_big = "2023";
-        if (durationArr) {
-          if (durationArr.value === "1") {
-            duration_small = "1";
-            duration_big = "1";
-          } else if (durationArr.value === "2-5") {
-            duration_small = "2";
-            duration_big = "5";
-          } else if (durationArr.value === "5-10") {
-            duration_small = "5";
-            duration_big = "10";
-          } else if (durationArr.value === "10") {
-            duration_small = "10";
-            duration_big = "2023";
-          } else if (durationArr.value === "0-30") {
-            duration_small = "0";
-            duration_big = "30";
-          } else if (durationArr.value === "30-60") {
-            duration_small = "30";
-            duration_big = "60";
-          } else if (durationArr.value === "60-90") {
-            duration_small = "60";
-            duration_big = "90";
-          } else if (durationArr.value === "90-120") {
-            duration_small = "90";
-            duration_big = "120";
-          } else if (durationArr.value === "120-150") {
-            duration_small = "120";
-            duration_big = "150";
-          } else if (durationArr.value === "150") {
-            duration_small = "150";
-            duration_big = "2023";
-          }
-        }
-    
-        var countryValArr = [];
-        countryArr?.forEach((element) => {
-          countryValArr.push(element.value);
-        });
-    
-        var genreValArr = [];
-        genreArr?.forEach((element) => {
-          genreValArr.push(element.value);
-        });
-        setTimeout(() => {
-          var jsonData1 = {
-            data: [
-              {
-                title: title,
-                year_small: releaseYearFrom ? releaseYearFrom : 0,
-                year_big: releaseYearTo ? releaseYearTo : 2023,
-                platform_netflix: isCheckedNetflix,
-                platform_amazon: isCheckedAmazon,
-                genres: genreArr ? genreValArr : "",
-                type: type, // "0" -> TV Show / "1" -> Movie
-                duration_small: duration_small,
-                duration_big: duration_big,
-                countries: countryArr ? countryValArr : "",
-                directors: director,
-                actors: actor,
-                flag: 0,
-                uid: parseInt(uid),
-              },
-            ],
-          };
-    
-          const result1 = searchFromWatchWishListAction(jsonData1).then((a) =>
-            setWatchedListInfo(a)
-          );
-          console.log(result1);
-    
-          //   setWatchedListInfo(result1);
-        }, 200);
-    
-        setTimeout(() => {
-          var jsonData2 = {
-            data: [
-              {
-                title: title,
-                year_small: releaseYearFrom ? releaseYearFrom : 0,
-                year_big: releaseYearTo ? releaseYearTo : 2023,
-                platform_netflix: isCheckedNetflix,
-                platform_amazon: isCheckedAmazon,
-                genres: genreArr ? genreValArr : "",
-                type: type, // "0" -> TV Show / "1" -> Movie
-                duration_small: duration_small,
-                duration_big: duration_big,
-                countries: countryArr ? countryValArr : "",
-                directors: director,
-                actors: actor,
-                flag: 1,
-                uid: parseInt(uid),
-              },
-            ],
-          };
-          const result2 = searchFromWatchWishListAction(jsonData2).then((r) =>
-            setWishlistInfo(r)
-          );
-          //   setWishlistInfo(result2);
-        }, 500);
-  }
+  const onFilterBoth = () => {
+    let duration_small = "0";
+    let duration_big = "2023";
+    if (durationArr) {
+      if (durationArr.value === "1") {
+        duration_small = "1";
+        duration_big = "1";
+      } else if (durationArr.value === "2-5") {
+        duration_small = "2";
+        duration_big = "5";
+      } else if (durationArr.value === "5-10") {
+        duration_small = "5";
+        duration_big = "10";
+      } else if (durationArr.value === "10") {
+        duration_small = "10";
+        duration_big = "2023";
+      } else if (durationArr.value === "0-30") {
+        duration_small = "0";
+        duration_big = "30";
+      } else if (durationArr.value === "30-60") {
+        duration_small = "30";
+        duration_big = "60";
+      } else if (durationArr.value === "60-90") {
+        duration_small = "60";
+        duration_big = "90";
+      } else if (durationArr.value === "90-120") {
+        duration_small = "90";
+        duration_big = "120";
+      } else if (durationArr.value === "120-150") {
+        duration_small = "120";
+        duration_big = "150";
+      } else if (durationArr.value === "150") {
+        duration_small = "150";
+        duration_big = "2023";
+      }
+    }
+
+    var countryValArr = [];
+    countryArr?.forEach((element) => {
+      countryValArr.push(element.value);
+    });
+
+    var genreValArr = [];
+    genreArr?.forEach((element) => {
+      genreValArr.push(element.value);
+    });
+    setTimeout(() => {
+      var jsonData1 = {
+        data: [
+          {
+            title: title,
+            year_small: releaseYearFrom ? releaseYearFrom : 0,
+            year_big: releaseYearTo ? releaseYearTo : 2023,
+            platform_netflix: isCheckedNetflix,
+            platform_amazon: isCheckedAmazon,
+            genres: genreArr ? genreValArr : "",
+            type: type, // "0" -> TV Show / "1" -> Movie
+            duration_small: duration_small,
+            duration_big: duration_big,
+            countries: countryArr ? countryValArr : "",
+            directors: director,
+            actors: actor,
+            flag: 0,
+            uid: parseInt(uid),
+          },
+        ],
+      };
+
+      const result1 = searchFromWatchWishListAction(jsonData1).then((a) =>
+        setWatchedListInfo(a)
+      );
+      console.log(result1);
+
+      //   setWatchedListInfo(result1);
+    }, 200);
+
+    setTimeout(() => {
+      var jsonData2 = {
+        data: [
+          {
+            title: title,
+            year_small: releaseYearFrom ? releaseYearFrom : 0,
+            year_big: releaseYearTo ? releaseYearTo : 2023,
+            platform_netflix: isCheckedNetflix,
+            platform_amazon: isCheckedAmazon,
+            genres: genreArr ? genreValArr : "",
+            type: type, // "0" -> TV Show / "1" -> Movie
+            duration_small: duration_small,
+            duration_big: duration_big,
+            countries: countryArr ? countryValArr : "",
+            directors: director,
+            actors: actor,
+            flag: 1,
+            uid: parseInt(uid),
+          },
+        ],
+      };
+      const result2 = searchFromWatchWishListAction(jsonData2).then((r) =>
+        setWishlistInfo(r)
+      );
+      //   setWishlistInfo(result2);
+    }, 500);
+  };
   const onSearch = async () => {
     let duration_small = "0";
     let duration_big = "2023";
@@ -828,7 +828,7 @@ const ProfilePage = () => {
             class="btn btn-primary btn-lg btn-block"
             id="filterWatched"
             style={{ "font-size": 20, "margin-top": "20px" }}
-            onClick={onFilterWatched()}
+            onClick={onFilterWatched}
             className="filterButton"
           >
             Filter your Watched List
@@ -839,7 +839,7 @@ const ProfilePage = () => {
             class="btn btn-primary btn-lg btn-block"
             id="filterWish"
             style={{ "font-size": 20, "margin-top": "20px" }}
-            onClick={onFilterWish()}
+            onClick={onFilterWish}
             value
             className="filterButton"
           >
@@ -851,7 +851,7 @@ const ProfilePage = () => {
             class="btn btn-primary btn-lg btn-block"
             id="filterBoth"
             style={{ "font-size": 20, "margin-top": "20px" }}
-            onClick={onFilterBoth()}
+            onClick={onFilterBoth}
             className="filterButton"
           >
             Filter Both
@@ -947,12 +947,12 @@ const ProfilePage = () => {
                 ) : null}
               </div>
             ))}
-            <FilmDetailModal
-        show={showModal}
-        modalShow={showModal}
-        selectedFilm={selected}
-        onHide={() => setShowModal(false)}
-      />
+          {/* <FilmDetailModal
+            show={showModal}
+            modalShow={showModal}
+            selectedFilm={selected}
+            onHide={() => setShowModal(false)}
+          /> */}
         </div>
         <div className="wishlistWrapper">
           <h5>Your Wishlist</h5>
@@ -1041,12 +1041,12 @@ const ProfilePage = () => {
                   ) : null}
                 </div>
               ))}
-              <FilmDetailModal
-        show={showModal}
-        modalShow={showModal}
-        selectedFilm={selected}
-        onHide={() => setShowModal(false)}
-      />
+            <FilmDetailModal
+              show={showModal}
+              modalShow={showModal}
+              selectedFilm={selected}
+              onHide={() => setShowModal(false)}
+            />
           </div>
         </div>
       </div>
